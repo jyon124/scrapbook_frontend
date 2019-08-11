271da36aafe5f526cd6e95d53283a19355b56d76
Jooyon Won <jyon1204@gmail.com>
2019-08-11 14:14:17 -0500

    CSS

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -9,7 +9,7 @@ render(){
     <img className="newsImg" src={this.props.news.urlToImage} />
     <div className="card__info">
       <span className="card__category">{this.props.news.category}</span>
-      <h3 className="card__title">{this.props.news.title}</h3>
+      <h4 className="card__title">{this.props.news.title}</h4>
        <span className="card__by">
          <a href="#" className="card__link" title="author">View Full Coverage</a>
       </span>
