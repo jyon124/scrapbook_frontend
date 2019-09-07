acc826f3cae751d3b4efaa5358377c0954babb28
Jooyon Won <jyon1204@gmail.com>
2019-09-07 17:33:09 -0400

    About CSS, Header Title

--- src/containers/about.js
+++ src/containers/about.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
+import headerImg from '../aboutus.jpg';
+
 
 class About extends React.Component {
 
     render(){
         return(
-            <div className="about-container">
-                <h1 className="about">About</h1>
+            <div>
+                <div className="head-img-container">
+                    <img alt="headerImg" className="about-header" src={headerImg} />
+                </div>
 
-                <p className="about-content1">Our mission is to increase the public's interest and understanding of what's happening in the world.</p>
+                <div className="about-container">
+                    <p className="about-content1">
+                        This page helps people to gain interest and to understand about the issues happening around the world.
+                    </p>
 
-                <h2 className="about-how"><i>How?</i></h2>
+                    <div className="about-color-box"></div>
+                    <h2 className="about-how"><i>How?</i></h2>
+                    <div className="how-underline"></div>
 
-                <p className="about-content2">
-                    This page eases access to news for any reader and provide various way to assist better understanding of content 
-                    by allowing reader to save choice of post into their scrapbook page.
-                </p>
+                    <p className="about-content2">
+                        This page allows access to news to anyone whose interested in viewing the world news on a single website. It provides various
+                        ways to assist better understanding of the contents by allowing the readers to save any wished posts into their scrapbook page.
+                    </p>
+                </div>
             </div>
         )
     }
