02711852bde7077b8ca590b613e0de009b583862
Jooyon Won <jyon1204@gmail.com>
2019-09-15 16:04:11 -0400

    Checked API

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,7 @@
 // const API_URL = 'http://localhost:3001/';
 const API_URL = 'https://scrapbook-backend.herokuapp.com/';
 
+
 export default {
     login: (loginData) => {
         const reqObj = {
